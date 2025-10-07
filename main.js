b966474e87b2c16ab0c93a040be9fe7d36247872
luizfernandofolchini <147443269+luizfernandofolchini@users.noreply.github.com>
2025-10-07 14:57:51 -0300

    07/10

--- main.js
+++ main.js
@@ -84,4 +84,17 @@ function mostraPergunta(){
     caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 
+function mostraAlternativa(){
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener('click', function(){
+            atual++;
+            mostraPergunta();
+        })
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+
 mostraPergunta();
