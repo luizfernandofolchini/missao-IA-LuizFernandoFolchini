7439ec233ec07606775aec1f0680bd4e599daa94
luizfernandofolchini <147443269+luizfernandofolchini@users.noreply.github.com>
2025-10-21 14:13:40 -0300

    21/10

--- main.js
+++ main.js
@@ -85,11 +85,11 @@ function mostraPergunta(){
     mostraAlternativas();
 }
 
-function mostraAlternativas(){
+function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener('click',function(){
+        botaoAlternativas.addEventListener('click', function () {
             atual++;
             mostraPergunta();
         })
